2d0841ead3583a5fcab754e001b2c8537010475b
vintagegold <vintagegold123@gmail.com>
2021-11-18 20:11:32 -0500

    Feature Engineering Functions

---
+++ helpers/fengine.py
@@ -0,0 +1,49 @@
+class Fengine():
+
+    def outlier_thresh(threshhold=3):
+
+        threshold_amount = threshold*df["target_COV"].std()
+
+        df["target_COV"] = df["target_COV"].mean()+threshold_amount
+
+        df = df.rename(columns={"target_COV": {f"target_COV_OT_{threshold}"}})
+
+        return df
+
+    def col_startswith(df, startswith_criteria="R_"):
+
+        df_change = df[[col for col in df.columns
+                        if col.startswith(startswith_criteria)
+                        and not col == "year.1"]]
+
+        return df_change
+
+    def col_endswith(df, endswith_criteria="R_"):
+
+        df_change = df[[col for col in df.columns
+                        if col.endswith(endswith_criteria)
+                        and not col == "year.1"]]
+
+        return df_change
+
+    def col_not_startswith(df, startswith_criteria="R_"):
+
+        df_change = df[[col for col in df.columns
+                        if not col.startswith(startswith_criteria)
+                        and not col == "year.1"]]
+
+    return df_change
+
+    def col_not_endswith(df, endswith_criteria="R_"):
+
+        df_change = df[[col for col in df.columns
+                        if not col.endswith(endswith_criteria)
+                        and not col == "year.1"]]
+
+        return df_change
+
+    def solar_mwh_hot_months(df, column_name=""):
+
+        df[column_name] = df['Hot Summer Months'] * df['Agg_R_Solar_MWh']
+
+        return df
